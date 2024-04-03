748608af84df03e8e40ba0ef51bec43fd29fc5a3
Balázs Faragó <54551879+Szpoti@users.noreply.github.com>
2024-04-03 11:37:44 +0200

    Add Formatting = Formatting.Indented for better readability

--- WabiSabiMonitor.ApplicationCore/Utils/WabiSabi/Models/Serialization/JsonSerializationOptions.cs
+++ WabiSabiMonitor.ApplicationCore/Utils/WabiSabi/Models/Serialization/JsonSerializationOptions.cs
@@ -8,41 +8,42 @@ namespace WabiSabiMonitor.ApplicationCore.Utils.WabiSabi.Models.Serialization;
 
 public class JsonSerializationOptions
 {
-	public static readonly JsonSerializerSettings CurrentSettings = new()
-	{
-		Converters = new List<JsonConverter>()
-			{
-				new ScalarJsonConverter(),
-				new GroupElementJsonConverter(),
-				new OutPointJsonConverter(),
-				new WitScriptJsonConverter(),
-				new ScriptJsonConverter(),
-				new OwnershipProofJsonConverter(),
-				new NetworkJsonConverter(),
-				new FeeRateJsonConverter(),
-				new MoneySatoshiJsonConverter(),
-				new Uint256JsonConverter(),
-				new Uint256DictionaryJsonConverter(),
-				new MultipartyTransactionStateJsonConverter(),
-				new ExceptionDataJsonConverter(),
-				new ExtPubKeyJsonConverter(),
-				new TimeSpanJsonConverter(),
-				new CoinJsonConverter(),
-				new CoinJoinEventJsonConverter(),
-				new GroupElementVectorJsonConverter(),
-				new ScalarVectorJsonConverter(),
-				new IssuanceRequestJsonConverter(),
-				new CredentialPresentationJsonConverter(),
-				new ProofJsonConverter(),
-				new MacJsonConverter()
-			}
-	};
+    public static readonly JsonSerializerSettings CurrentSettings = new()
+    {
+        Converters = new List<JsonConverter>()
+            {
+                new ScalarJsonConverter(),
+                new GroupElementJsonConverter(),
+                new OutPointJsonConverter(),
+                new WitScriptJsonConverter(),
+                new ScriptJsonConverter(),
+                new OwnershipProofJsonConverter(),
+                new NetworkJsonConverter(),
+                new FeeRateJsonConverter(),
+                new MoneySatoshiJsonConverter(),
+                new Uint256JsonConverter(),
+                new Uint256DictionaryJsonConverter(),
+                new MultipartyTransactionStateJsonConverter(),
+                new ExceptionDataJsonConverter(),
+                new ExtPubKeyJsonConverter(),
+                new TimeSpanJsonConverter(),
+                new CoinJsonConverter(),
+                new CoinJoinEventJsonConverter(),
+                new GroupElementVectorJsonConverter(),
+                new ScalarVectorJsonConverter(),
+                new IssuanceRequestJsonConverter(),
+                new CredentialPresentationJsonConverter(),
+                new ProofJsonConverter(),
+                new MacJsonConverter()
+            },
+        Formatting = Formatting.Indented
+    };
 
-	public static readonly JsonSerializationOptions Default = new();
+    public static readonly JsonSerializationOptions Default = new();
 
-	private JsonSerializationOptions()
-	{
-	}
+    private JsonSerializationOptions()
+    {
+    }
 
-	public JsonSerializerSettings Settings => CurrentSettings;
+    public JsonSerializerSettings Settings => CurrentSettings;
 }
