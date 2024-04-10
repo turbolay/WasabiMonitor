fff24afc0b02791f465bfc5bffa5ee4ddffa870e
Balázs Faragó <54551879+Szpoti@users.noreply.github.com>
2024-04-10 16:23:10 +0200

    Modify RoundStateStoreManager to use RoundsDataFilter

--- WabiSabiMonitor.ApplicationCore/RoundStateStoreManager.cs
+++ WabiSabiMonitor.ApplicationCore/RoundStateStoreManager.cs
@@ -6,55 +6,54 @@ using WabiSabiMonitor.ApplicationCore.Interfaces;
 using WabiSabiMonitor.ApplicationCore.Utils.Helpers;
 using WabiSabiMonitor.ApplicationCore.Utils.Logging;
 using WabiSabiMonitor.ApplicationCore.Utils.WabiSabi.Backend.Rounds;
+using WabiSabiMonitor.ApplicationCore.Utils.WabiSabi.Models;
 using WabiSabiMonitor.ApplicationCore.Utils.WabiSabi.Models.Serialization;
 
 namespace WabiSabiMonitor.ApplicationCore
 {
     public class RoundStateStoreManager : BackgroundService
     {
-        private readonly IRoundDataReaderService _roundDataReaderService;
+        private readonly IRoundsDataFilter _roundsDataFilter;
 
-        public RoundStateStoreManager(IRoundDataReaderService roundDataReaderService)
+        public RoundStateStoreManager(IRoundsDataFilter roundsDataFilter)
         {
-            _roundDataReaderService = roundDataReaderService;
+            _roundsDataFilter = roundsDataFilter;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            List<KeyValuePair<uint256, RoundDataReaderService.ProcessedRound>> savedForNextDay = new();
+            List<RoundState> savedForNextDay = new();
             while (!stoppingToken.IsCancellationRequested)
             {
                 var date = DateTime.Now.Date;
                 await WaitUntilMidnightAsync(stoppingToken);
                 Logger.LogInfo("It's midnight. Writing Daily Round States...");
 
-                Dictionary<uint256, RoundDataReaderService.ProcessedRound> allRounds = _roundDataReaderService.Rounds;
-
                 // Get rounds that happened today.
-                var roundsToday = allRounds.Where(x => x.Value.LastUpdate.Date == date).ToList();
+                var roundsToday = _roundsDataFilter.GetRoundsInInterval(date, date.AddDays(1)).ToList();
 
                 // Add leftovers from yesterday.
                 roundsToday.AddRange(savedForNextDay);
                 savedForNextDay.Clear();
 
                 // Save finished rounds.
-                var finishedRounds = roundsToday.Where(x => x.Value.Round.EndRoundState != EndRoundState.None);
+                var finishedRounds = _roundsDataFilter.GetRoundsFinishedSince(date).ToArray();
 
                 // If a BlameRound is still active, don't save its BlameOf.
-                var stillActiveBlameRoundsBlameOfIds = roundsToday.Where(x => x.Value.Round.EndRoundState == EndRoundState.None && x.Value.Round.IsBlame())
-                    .Select(x => x.Value.Round.BlameOf);
+                var stillActiveBlameRoundsBlameOfIds = roundsToday.Where(x => x.EndRoundState == EndRoundState.None && x.IsBlame())
+                    .Select(x => x.BlameOf).ToArray();
 
                 var dataToWrite = finishedRounds.Where(round =>
                 {
-                    bool isBlameOf = stillActiveBlameRoundsBlameOfIds.Contains(round.Key);
+                    bool isBlameOf = stillActiveBlameRoundsBlameOfIds.Contains(round.Id);
                     if (isBlameOf)
                     {
-                        Logger.LogInfo($"Excluded Round {round.Key}, is BlameOf.");
+                        Logger.LogInfo($"Excluded Round {round.Id}, is BlameOf.");
                         savedForNextDay.Add(round);
                     }
 
                     return !isBlameOf;
-                });
+                }).ToArray();
 
                 var path = Path.Combine(EnvironmentHelpers.GetDataDir(Path.Combine("WabiSabiMonitor", "Client")), $"RoundStates_{date:yyyy-MM-dd}.json");
                 await File.WriteAllTextAsync(path, JsonConvert.SerializeObject(dataToWrite, JsonSerializationOptions.CurrentSettings), stoppingToken);
