9bef6011109f17578c90698ae64662c03a8169d5
Balázs Faragó <54551879+Szpoti@users.noreply.github.com>
2024-04-11 11:03:39 +0200

    Add base of AnalysisStoreManager

---
+++ WabiSabiMonitor.ApplicationCore/AnalysisStoreManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using WabiSabiMonitor.ApplicationCore.Interfaces;
+using WabiSabiMonitor.ApplicationCore.Utils.Bases;
+
+namespace WabiSabiMonitor.ApplicationCore
+{
+    public class AnalysisStoreManager : PeriodicRunner
+    {
+        private readonly IAnalyzer _analyzer;
+        private readonly IRoundsDataFilter _roundsDataFilter;
+
+        public AnalysisStoreManager(IAnalyzer analyzer, IRoundsDataFilter roundsDataFilter, TimeSpan period) : base(period)
+        {
+            _analyzer = analyzer;
+            _roundsDataFilter = roundsDataFilter;
+        }
+
+        protected override async Task ActionAsync(CancellationToken cancel)
+        {
+            while (!cancel.IsCancellationRequested)
+            {
+                var date = DateTime.Now.Date;
+                var roundStates = _roundsDataFilter.GetRoundsStartedSince(date);
+                var analysis = _analyzer.AnalyzeRoundStates(roundStates);
+            }
+        }
+    }
+}
